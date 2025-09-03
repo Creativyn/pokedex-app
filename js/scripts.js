1274db94f3cc960540ac83ffe858ca086e1eb92c
Corey Gavyn <cgavyn8@outlook.com>
2025-09-02 23:15:45 -0700

    Updated and improved JS language
    Added arrays to all Types and properly formatted the JS.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-let pokemonList = [{name: 'Ninetales', height: '1', type: 'fire'}, {name: 'Fearow', height: '1', type: ['flying', 'normal']}, {name: 'Arbok', height: '3', type: 'poison'}, {name: 'Scyther', height: '1', type: ['bug', 'flying']}, {name: 'Umbreon', height: '1', type: 'dark'}, {name: 'Lugia', height: '5', type: ['psychic','flying']}]
+let pokemonList = [
+  { name: "Ninetales", height: "1", types: ["fire"] },
+  { name: "Fearow", height: "1", types: ["flying", "normal"] },
+  { name: "Arbok", height: "3", types: ["poison"] },
+  { name: "Scyther", height: "1", types: ["bug", "flying"] },
+  { name: "Umbreon", height: "1", types: ["dark"] },
+  { name: "Lugia", height: "5", types: ["psychic", "flying"] },
+];
