d7d6457587a3c8163f8cf83a09ea17af8aa44fb4
Corey Gavyn <cgavyn8@outlook.com>
2025-09-10 23:41:32 -0700

    Added forEach, IIFE, and filter for search.
    Changed for loop to forEach. Created a pokemonRepository and assigned it to the IIFE. Attempted to ensure that the typeof is an object and add a filter for searching.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,33 @@
+let pokemonRepository = (function () {
+  let pokemonList = []; //empty array
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+
+    function isObject(value) {
+      return value !== null && typeof value === "object";
+    } // checks if the typeof parameter is an object
+  }
+  // if ((isObject = true)) {
+  //   return pokemonList;
+  // } else {
+  //   return null;
+  // } // the bonus task said 'In combination with a conditional, make sure you can only add the passed argument of the function to pokemonList if it’s an object. This was the only way I could think of to do this. However, it wiped my list so I commented it out.
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
+
+document.write(pokemonRepository.getAll()); //[]
+pokemonRepository.add("Venusaur");
+document.write(pokemonRepository.getAll()); // [ { name: "Venusaur"} ]
+
 let pokemonList = [
   {
     name: "Ninetales",
@@ -31,12 +61,24 @@ let pokemonList = [
   },
 ];
 
-for (let i = 0; i < pokemonList.length; i++) {
-  let pokemon = pokemonList[i];
-  document.write(`${pokemon.name} (height: ${pokemon.height})`);
+// for (let i = 0; i < pokemonList.length; i++) {
+//   let pokemon = pokemonList[i];
+//   document.write(`${pokemon.name} (height: ${pokemon.height})`);
+// }
 
-  if (pokemon.height > 4) {
-    document.write(" - Wow, that's big!");
-  }
+function myLoopFunction(pokemon) {
+  document.write("<p>" + pokemon.name + " " + pokemon.height + "<p>");
+}
+
+// function filterPokemon(arr, query) {
+//   return arr.filter((el) => el.toLowerCase().includes(query.toLowerCase()));
+// }
+
+// console.log(filterPokemon(pokemon, "Lug")); //My attempt to filter. Wiped everything but Venusaur.
+
+pokemonList.forEach(myLoopFunction);
+
+if (pokemon.height > 4) {
+  document.write(" - Wow, that's big!");
   document.write("<br>");
 }
