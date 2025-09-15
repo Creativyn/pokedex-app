405c385424595416e864a611fa765bbea03b4be4
Corey Gavyn <cgavyn8@outlook.com>
2025-09-15 11:21:19 -0700

    Streamlined getAll and added addListItem and eventListener

--- js/scripts.js
+++ js/scripts.js
@@ -30,6 +30,11 @@ let pokemonRepository = (function () {
       height: "5.2",
       types: ["psychic", "flying"],
     },
+    {
+      name: "Venusaur",
+      height: "2",
+      types: ["grass", "poison"],
+    },
   ];
 
   function add(pokemon) {
@@ -40,19 +45,30 @@ let pokemonRepository = (function () {
     return pokemonList;
   }
 
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listPokemon = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listPokemon.appendChild(button);
+    pokemonList.appendChild(listPokemon);
+    button.addEventListener("click", function (showDetails) {
+      console.log(pokemon.name);
+    }); //shows more details about a pokemon when it's name is clicked
+  }
+
+  // function showDetails(pokemon) {
+  //   console.log();
+  // }
+
   return {
     add: add,
     getAll: getAll,
+    addListItem: addListItem,
   };
 })();
 
-pokemonRepository.getAll().forEach((pokemon) => {
-  document.write(pokemon.name + " " + pokemon.height);
-  if (pokemon.height > 4) {
-    document.write(" - Wow, that's big!");
-  }
-  document.write("<br>");
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
 });
-
-pokemonRepository.add("Venusaur");
-document.write(pokemonRepository.getAll()); // [ { name: "Venusaur"} ]
