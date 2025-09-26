9c76ebae34dd2641465e3a8561d4028a361acfc4
Corey Gavyn <cgavyn8@outlook.com>
2025-09-26 15:21:04 -0700

    Attempted to add modal that would show the pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+let modalContainer = document.querySelector("#modal-container");
 let pokemonRepository = (function () {
   let pokemonList = [];
   let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=151";
@@ -32,26 +33,24 @@ let pokemonRepository = (function () {
   }
 
   function showDetails(pokemon) {
-    console.log(pokemon);
+    showModal(pokemon.name, pokemon.name + pokemon.height + pokemon.img);
   }
 
   function showLoadingMessage(message = "Page is loading.") {
-    const container = document.getElementById("loading-container");
-    const messageDiv = document.getElementById("loading-message");
-    messageDiv.textContent = message;
+    const container = document.getElementById("modal-container");
+    container.innerText = message;
     container.style.display = "block";
   }
 
   function hideLoadingMessage() {
-    const container = document.getElementById("loading-container");
+    const container = document.getElementById("modal-container");
     container.style.display = "none";
   }
 
   function loadList() {
-    showLoadingMessage;
+    showLoadingMessage();
     return fetch(apiUrl)
       .then(function (response) {
-        hideLoadingMessage;
         return response.json(); //Returns a promise
       })
       .then(function (json) {
@@ -61,20 +60,20 @@ let pokemonRepository = (function () {
             detailsUrl: item.url,
           };
           add(pokemon);
+          hideLoadingMessage();
         });
       })
       .catch(function (e) {
-        hideLoadingMessage;
+        hideLoadingMessage();
         console.error(e);
       });
   }
 
   function loadDetails(item) {
-    showLoadingMessage;
+    showLoadingMessage();
     let url = item.detailsUrl;
     return fetch(url)
       .then(function (response) {
-        hideLoadingMessage;
         return response.json();
       })
       .then(function (details) {
@@ -82,16 +81,17 @@ let pokemonRepository = (function () {
         item.imageUrl = details.sprites.front_default;
         item.height = details.height;
         item.types = details.types;
+        hideLoadingMessage();
       })
       .catch(function (e) {
-        hideLoadingMessage;
+        hideLoadingMessage();
         console.error(e);
       });
   }
 
   function showDetails(pokemon) {
     loadDetails(pokemon).then(function () {
-      console.log(pokemon);
+      showModal(pokemon.name + pokemon.height + pokemon.img);
     });
   }
 
@@ -110,3 +110,54 @@ pokemonRepository.loadList().then(function () {
     pokemonRepository.addListItem(pokemon);
   });
 });
+
+(function () {
+  function showModal(title, text) {
+    // Clear all existing modal content
+    modalContainer.innerHTML = "";
+
+    let modal = document.createElement("div");
+    modal.classList.add("modal");
+
+    // Add the new modal content
+    let closeButtonElement = document.createElement("button");
+    closeButtonElement.classList.add("modal-close");
+    closeButtonElement.innerText = "Close";
+    closeButtonElement.addEventListener("click", hideModal);
+
+    let titleElement = document.createElement("h1");
+    titleElement.innerText = title;
+
+    let contentElement = document.createElement("p");
+    contentElement.innerText = text;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modalContainer.appendChild(modal);
+
+    modalContainer.classList.add("is-visible");
+
+    modalContainer.addEventListener("click", (e) => {
+      // Since this is also triggered when clicking INSIDE the modal
+      // We only want to close if the user clicks directly on the overlay
+      let target = e.target;
+      if (target === modalContainer) {
+        hideModal();
+      }
+    });
+  }
+
+  let dialogPromiseReject; //This can be set later, by showDialog
+
+  function hideModal() {
+    modalContainer.classList.remove("is-visible");
+
+    if (dialogPromiseReject) {
+      dialogPromiseReject();
+      dialogPromiseReject = null;
+    }
+  }
+
+  // THE RETURN STATEMENT HERE
+})();
